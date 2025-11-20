99aefdaa09ab327858801e4c87a1b5af1ee91b79
Dylan Shah <dylan.shah@outlook.com>
2025-11-19 23:00:13 -0700

    Scenes

--- src/main/java/edu/asu/dylan/platformergame/Initialise.java
+++ src/main/java/edu/asu/dylan/platformergame/Initialise.java
@@ -9,17 +9,17 @@ import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
-public class SirensShore extends Application {
+public class Initialise extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Settings.stage = stage;
-        FXMLLoader fxmlLoader = new FXMLLoader(SirensShore.class.getResource("Start.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Initialise.class.getResource("Start.fxml"));
         Scene startScene = new Scene(fxmlLoader.load(), 1900, 1000);
         stage.setOnCloseRequest((WindowEvent _) -> {
             Platform.exit();
             System.exit(0);
         });
-        stage.setTitle("Siren's Shore!");
+        stage.setTitle("Bee Flight Game!");
         stage.setScene(startScene);
         stage.show();
         stage.setResizable(false);
--- src/main/java/edu/asu/dylan/platformergame/Start.java
+++ src/main/java/edu/asu/dylan/platformergame/Start.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 public class Start {
     @FXML
     protected void onStartButtonClick() {
-        System.out.println("Start.onStartButtonClick");
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/edu/asu/dylan/platformergame/Game.fxml"));
         try {
             Scene gameScene = new Scene(fxmlLoader.load(), 1900, 1000);
@@ -17,9 +18,7 @@ public class Start {
             Settings.stage.show();
             Game game = fxmlLoader.getController();
             game.StartGame();
-        } catch (IOException e) {
-            System.out.println("e = " + e);
-            e.printStackTrace();
+        } catch (IOException _) {
         }
     }
 }
